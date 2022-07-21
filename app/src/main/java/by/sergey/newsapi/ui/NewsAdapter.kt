80c3dafdc38bdd02ad3303313641b32ea72c0d9a
Sergey <serg.karpik@gmail.com>
2022-07-21 15:34:11 +0300

    adapter finished

--- app/src/main/java/by/sergey/newsapi/ui/NewsAdapter.kt
+++ app/src/main/java/by/sergey/newsapi/ui/NewsAdapter.kt
@@ -1,15 +1,19 @@
 package by.sergey.newsapi.ui
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import by.sergey.newsapi.R
+import by.sergey.newsapi.databinding.NewsListItemBinding
 import by.sergey.newsapi.models.Article
+import com.bumptech.glide.Glide
 
 class NewsAdapter:RecyclerView.Adapter<NewsAdapter.NewsViewHolder>() {
 
-    inner class NewsViewHolder(itemView: View):RecyclerView.ViewHolder(itemView)
+    inner class NewsViewHolder(val binding: NewsListItemBinding):RecyclerView.ViewHolder(binding.root)
 
     private val diffCallback = object : DiffUtil.ItemCallback<Article>()
     {
@@ -29,11 +33,20 @@ class NewsAdapter:RecyclerView.Adapter<NewsAdapter.NewsViewHolder>() {
     fun submitList(list: List<Article>) = differ.submitList(list)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsAdapter.NewsViewHolder {
-        TODO("Not yet implemented")
+       val binding = NewsListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return NewsViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: NewsAdapter.NewsViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val item = differ.currentList[position]
+
+        holder.apply {
+            binding.title.text = item.title;
+            binding.description.text = item.description;
+            Glide.with(holder.itemView.context)
+                .load(item.urlToImage)
+                .into(binding.icon)
+        }
     }
 
     override fun getItemCount(): Int {
